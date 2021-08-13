e870f8d989f4dd8991084d1a8619b5127161e0e8
Shakhan <khan.vinn@gmail.com>
2021-08-13 16:04:38 +0500

    base jest test methods

--- tests/auth-routes.test.js
+++ tests/auth-routes.test.js
@@ -1,5 +1,25 @@
+
 describe('Sample Test', () => {
     it('should test that true === true', () => {
         expect(true).toBe(true)
+    })
+    it("udefined", () => {
+        expect(undefined).toBe(undefined)
+    })
+})
+
+function add(x = 0, y = 0) {
+    return x + y
+}
+
+describe("Add function test", () => {
+    it("add two numbers", () => {
+        expect(add(1, 3)).toBe(4)
+    })
+    it("add only one numbers", () => {
+        expect(add(5)).toBe(5)
+    })
+    it("add no nubers", () => {
+        expect(add()).toBe(0)
     })
 })
