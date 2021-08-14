da73455ec9607d097f06481e5be7b38f61cf705c
Shakhan <khan.vinn@gmail.com>
2021-08-14 10:39:18 +0500

    replace db connect function to main package it's comfortable for jest tests

--- app.js
+++ app.js
@@ -8,14 +8,6 @@ const userRouter = require('./routes/user');
 const app = express();
 
 appUtils(app)
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-  .then(() => console.log("connected to DB"))
-  .catch((e) => console.log(e))
 
 app.use('/', indexRouter);
 app.use('/_api/user', userRouter);
