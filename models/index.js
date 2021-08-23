c0e089d363bb6b4addb92a288d590316a9b9c0fc
Shakhan <khan.vinn@gmail.com>
2021-08-23 03:12:15 +0000

    new quize model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const quizeSchema = require("./schema/quize");
 const UserSchema = require("./schema/user");
 
 const User = mongoose.model("Users", UserSchema)
+const Quize = mongoose.model("Quize", quizeSchema)
 
-module.exports = { User }
+module.exports = { User, Quize }
