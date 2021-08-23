58306495e12e5ba8fbc12c7d771aceacbabbb25f
Shakhan <khan.vinn@gmail.com>
2021-08-23 03:12:35 +0000

    quize route

---
+++ routes/quize.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { userTokenVerify } = require('../middlewares/auth');
+const { userParamsTokenValidate } = require('../middlewares/user');
+const router = express.Router();
+
+router.post("/new", (req, res, next) => { })
+
+router.get("/all", (req, res, next) => { })
+
+router.get("/:id", (req, res, next) => { })
+
+router.put("/:id", (req, res, next) => { })
