45882f4b61fbf9f99fd23d209a08da8d1564f40f
Shakhan <khan.vinn@gmail.com>
2021-08-23 03:11:33 +0000

    new quize schema

---
+++ models/schema/quize.js
@@ -0,0 +1,33 @@
+const { nanoid } = require("nanoid")
+const mongoose = require("mongoose")
+
+const querySchema = mongoose.Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+    invalidAnswers: {
+        type: [String],
+        required: true,
+        minLength: 2
+    }
+})
+
+const quizeSchema = mongoose.Schema({
+    answers: {
+        type: [querySchema],
+        required: true,
+        minLength: 1,
+        maxLength: 25
+    },
+    belongsTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+}, { timeStamps: true })
+
+module.exports = quizeSchema
