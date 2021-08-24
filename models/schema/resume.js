05aa29afe89b6d65ec8f5c3a3920563c7ec000f4
Shakhan <khan.vinn@gmail.com>
2021-08-24 12:19:30 +0500

    resume chema

--- models/schema/resume.js
+++ models/schema/resume.js
@@ -1,9 +1,102 @@
 const mongoose = require('mongoose');
 
+const InstitutionLocationSchema = mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  city: {
+    type: String,
+    required: true,
+    minLength: 4,
+  },
+  street: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+});
+const EducationSchema = mongoose.Schema({
+  institutionFullName: {
+    type: String,
+    required: true,
+    minLength: 4,
+  },
+  location: {
+    type: InstitutionLocationSchema,
+    required: true,
+  },
+  year: {
+    type: [Number],
+    minLength: 2,
+    maxLength: 2,
+  },
+});
+const CompanySchema = mongoose.Schema({
+  name: {
+    type: String,
+  },
+  location: {
+    type: InstitutionLocationSchema,
+  },
+  employeesCount: {
+    type: Number,
+  },
+});
+const ExperienceSchema = mongoose.Schema({
+  comapany: {
+    type: CompanySchema,
+  },
+  year: {
+    type: [Number],
+    minLength: 2,
+    maxLength: 2,
+  },
+  responsibilities: {
+    type: [String],
+    minLength: 4,
+  },
+  skills: {
+    type: [String],
+  },
+});
+
+const UserSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  birthDate: {
+    type: Number,
+    required: true,
+  },
+  about: {
+    type: String,
+    required: true,
+    minLength: 40,
+  },
+  education: {
+    type: [EducationSchema],
+  },
+  experiences: {
+    type: [ExperienceSchema],
+  },
+});
+
 const ResumeSchema = mongoose.Schema({
   belengs_to: {
     type: mongoose.Types.ObjectId,
     required: true,
+  },
+  user: {
+    type: UserSchema,
   },
 }, { timestamps: { currentTime: () => Math.floor(Date.now() / 1000) } });
 
