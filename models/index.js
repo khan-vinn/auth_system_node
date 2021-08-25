6358b4a38d1256a1e4517f6501e9d383a4059ae9
Shakhan <khan.vinn@gmail.com>
2021-08-25 14:22:02 +0500

    Answer model to application

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const AnswerSchema = require('./schema/answer');
 const QuizeSchema = require('./schema/quize');
 const ResumeSchema = require('./schema/resume');
 const UserSchema = require('./schema/user');
@@ -6,5 +7,8 @@ const UserSchema = require('./schema/user');
 const User = mongoose.model('User', UserSchema);
 const Quize = mongoose.model('Quize', QuizeSchema);
 const Resume = mongoose.model('Resume', ResumeSchema);
+const Anser = mongoose.model('Answer', AnswerSchema);
 
-module.exports = { User, Quize, Resume };
+module.exports = {
+  User, Quize, Resume, Anser,
+};
