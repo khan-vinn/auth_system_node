c55407df7cb1a5502daa3c300b1f9d67cb35d681
Shakhan <khan.vinn@gmail.com>
2021-08-26 09:14:30 +0500

    comment logger

--- utils.js
+++ utils.js
@@ -1,19 +1,18 @@
-var logger = require('morgan');
-var express = require('express');
-// var cookieParser = require('cookie-parser');
-// var path = require('path');
-// var lessMiddleware = require('less-middleware');
+const express = require('express');
+// const logger = require('morgan');
+// const cookieParser = require('cookie-parser');
+// const path = require('path');
+// const lessMiddleware = require('less-middleware');
 
 module.exports = function (app) {
-    
-    app.use(logger('dev'));
-    app.use(express.json());
-    // app.use(cookieParser());
-    // app.use(express.urlencoded({ extended: false }));
+  // app.use(logger('dev'));
+  app.use(express.json());
+  // app.use(cookieParser());
+  // app.use(express.urlencoded({ extended: false }));
 
-    // app.set('views', path.join(__dirname, 'views'));
-    // app.set('view engine', 'pug');
-    
-    // app.use(lessMiddleware(path.join(__dirname, 'public')));
-    // app.use(express.static(path.join(__dirname, 'public')));
-}
+  // app.set('views', path.join(__dirname, 'views'));
+  // app.set('view engine', 'pug');
+
+  // app.use(lessMiddleware(path.join(__dirname, 'public')));
+  // app.use(express.static(path.join(__dirname, 'public')));
+};
