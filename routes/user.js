2e4ea87e2ff98c07f7063db93c10bc6179dd8e69
Shakhan <khan.vinn@gmail.com>
2021-08-28 15:05:29 +0500

    send in response latest item from user tokens to send any request, and delete token from db where routed to signout

--- routes/user.js
+++ routes/user.js
@@ -33,8 +33,10 @@ router.all('/signin',
           return generateAccessToken({ email, userAgent: req.get('User-Agent') });
         }
       })
-      .then((token) => User.findOneAndUpdate({ email }, { token }, { new: true }))
-      .then((user) => res.status(200).json({ status: 200, token: user.token }))
+      .then((token) => User.findOneAndUpdate({ email }, { $push: { token } }, { new: true }))
+      .then((user) => res.status(200).json(
+        { status: 200, token: user.token[user.token.length - 1] },
+      ))
       .catch((e) => res.status(500).json({ status: 500, message: `${e.name} :: ${e.message}` }));
   });
 
@@ -55,14 +57,17 @@ router.all('/signup',
         savedToken = token;
         return bcrypt.hash(password, 12);
       })
-      .then((hash) => User.create({ email, token: savedToken, password: hash }))
-      .then((user) => res.status(200).json({ status: 200, token: user.token }))
+      .then((hash) => User.create({ email, token: [savedToken], password: hash }))
+      .then((user) => res.status(200).json(
+        { status: 200, token: user.token[user.token.length - 1] },
+      ))
       .catch((e) => res.status(500).json({ status: 500, message: `${e.name} :: ${e.message}` }));
   });
 
 router.all('/signout', userParamsTokenValidate, userTokenVerify, (req, res) => {
+  const { token } = req.body;
   const { user } = res.locals.user;
-  User.findOneAndUpdate({ email: user.email }, { token: '' })
+  User.findOneAndUpdate({ email: user.email }, { $pull: { token } })
     .then(() => res.status(200).json({ status: 200, message: 'OK' }))
     .catch((e) => res.status(500).json({ status: 500, message: `${e.name} :: ${e.message}` }));
 });
